70ec4dceba9b3133f81c0ffd680310c78d4915b6
JonRol <1195532574@qq.com>
2020-07-31 18:26:55 +0800

    add user模块

---
+++ kmall-user/src/test/java/com/caesar/kmall/user/KmallUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caesar.kmall.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmallUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
